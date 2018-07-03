88fe1a212ce453d2609fac02c6c6af3d672b731d
ben holsinger <bholsinger@gmail.com>
2018-07-03 11:08:31 -0600

    fixed stars and timer - new bug
    - added value to all cards after the deck was made
    - added timer class to html
    ------------------------------------
    - new bug with enable and disable card function (trying to set the style to pointer-events )
    --------------------------------------
    Uncaught ReferenceError: Invalid left-hand side in assignment
    line 124

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ var openCards = [];
 // declaring move variable
 let moves = 0;
 let counter = document.querySelector(".moves");
+//declaring stars variable
 const stars = document.querySelectorAll(".fa-star");
 
 /*
@@ -80,7 +81,8 @@ function startGame() {
   });
   //this is where we put the value of the cards into the deck
   deck.innerHTML = cardHTML.join("");
-
+  allCards = document.querySelectorAll(".card");
+  //setting the amount of moves to 0
   moves = 0;
   counter.innerHTML = moves;
   //reset rating
@@ -107,13 +109,29 @@ function matched() {
 }
 
 function unmatched() {
+  disable();
   setTimeout(function() {
     openCards.forEach(function(card) {
       card.classList.remove("open", "show");
     });
+    enable();
     openCards = [];
   }, 1000);
 }
+
+
+function disable(){
+allCards.style.pointer-events= "none";
+}
+funtion enable() {
+allCards.style.pointer-events= "auto";
+}
+
+
+
+
+
+
 
 function moveCounter() {
   moves++;
@@ -160,6 +178,8 @@ function startTimer() {
   }, 1000);
 }
 
+
+//this event is not occuring -bug
 allCards.forEach(function(card) {
   card.addEventListener("click", function(e) {
     if (
